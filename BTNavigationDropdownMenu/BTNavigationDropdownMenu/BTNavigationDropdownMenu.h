1f162f871dce6ec0c6ce00a880706df21625b225
cmaster11 <cmaster11@gmail.com>
2018-08-03 06:28:47 +0300

    Upgraded to Swift 4.1, prepared for Carthage use

---
+++ BTNavigationDropdownMenu/BTNavigationDropdownMenu/BTNavigationDropdownMenu.h
@@ -0,0 +1,19 @@
+//
+//  BTNavigationDropdownMenu.h
+//  BTNavigationDropdownMenu
+//
+//  Created by cmaster11 on 03/08/2018.
+//  Copyright © 2018 hydex11.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BTNavigationDropdownMenu.
+FOUNDATION_EXPORT double BTNavigationDropdownMenuVersionNumber;
+
+//! Project version string for BTNavigationDropdownMenu.
+FOUNDATION_EXPORT const unsigned char BTNavigationDropdownMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BTNavigationDropdownMenu/PublicHeader.h>
+
+
